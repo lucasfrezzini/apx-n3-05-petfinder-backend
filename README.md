dafea0f78ce3511b6ab25923db65b66594a583ea
lucasfrezzini <luquiitas.f@gmail.com>
2025-02-11 21:39:06 -0300

    Petfinder backend: initial configuration with dependencies

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backend for the Petfinder project of the N3 level of the APX course.
+
+The project is a platform for adopting and report pets.
---
+++ src/client/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="p-8 text-center">
+    <h1 class="text-6xl font-bold underline text-fuchsia-800">
+      Hello Petfinder
+    </h1>
+  </div>
+`;
