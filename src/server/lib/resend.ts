a94438b7b9e6e1ee06db79d578b5cf4e1d257edc
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-20 03:05:22 -0300

    Feat: modificar función de envío de correos para usar destinatario dinámico

--- src/server/lib/resend.ts
+++ src/server/lib/resend.ts
@@ -5,14 +5,14 @@ const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export function sendEmailByResend(
   from: string,
-  _to: string,
+  to: string,
   subject: string,
   html: string
 ) {
   resend.emails.send({
     from: `${from} <onboarding@resend.dev>`,
-    // to: [to],
-    to: "luquiitas.f@gmail.com",
+    to: [to],
+    // to: "luquiitas.f@gmail.com",
     subject,
     html,
   });
